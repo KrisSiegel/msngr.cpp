b62696248e2d17a698c85e731e9ecdb09f2020de
Kris Siegel <krissiegel@gmail.com>
2018-12-29 03:52:32 -0800

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include "msngr.hpp"
 
+using namespace std;
+
 int main() {
+    // Create the instance of msngr that will be used throughout
     Msngr msngr;
 
-    msngr.on("topic1", []() {
-        std::cout << "topic1 subscriber" << std::endl;
+    // Now, how do we emit simple messages?
+    msngr.emit("MyTopic");
+
+    // Simple, right? But, you had no listeners for that particular topic so it never went anywhere
+    // So, let's setup one now!
+    msngr.on("MyTopic", []() {
+        cout << "The MyTopic listener was executed" << endl;
     });
 
-    msngr.emit("topic1");
-    msngr.emit("topic5", "cat2");
-    msngr.emit("topic3");
-    msngr.emit("topic3");
-    msngr.emit("topic1", "cat7");
+    // Great! A listener! Now let's emit again
+    msngr.emit("MyTopic");
+
+    // You should have gotten the console output "The MyTopic listener was executed"
+    // Woohoo!
+    // Don't forget to clean up the handler when you're done with it
+    msngr.drop("MyTopic");
+    
+    // But wait, there's more!
+    // What if you wanted to capture
 }
--- src/msngr.hpp
+++ src/msngr.hpp
@@ -18,93 +18,166 @@
 #include <string>
 #include <functional>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 using std::string;
 using std::function;
 using std::vector;
+using std::unordered_map;
+using std::transform;
 
+/*
+    Msngr
+
+*/
 class Msngr {
     public:
-        struct Message {
-            public:
-                Message() {};
-                Message(optional<string> topic, optional<string> category, optional<string> subcategory) : topicOpt(topic), categoryOpt(category), subcategoryOpt(subcategory) {};
-
-                optional<string> topicOpt = nullopt;
-                optional<string> categoryOpt = nullopt;
-                optional<string> subcategoryOpt = nullopt;
-        };
+        struct Message;
 
         Msngr() {};
         ~Msngr() {};
 
-        void emit(string topic) {
-            Message msg(topic, nullopt, nullopt);
-            internal_emit(msg);
-        };
+        void emit(string topic);
+        void emit(string topic, string category);
+        void emit(string topic, string category, string subcategory);
 
-        void emit(string topic, string category) {
-            Message msg(topic, category, nullopt);
-            internal_emit(msg);
-        };
+        void on(string topic, const function<void()> &fn);
+        void on(string topic, string category, const function<void()> &fn);
+        void on(string topic, string category, string subcategory, const function<void()> &fn);
 
-        void emit(string topic, string category, string subcategory) {
-            Message msg(topic, category, subcategory);
-            internal_emit(msg);
-        };
-
-        void emit(Message message) {
-            internal_emit(message);
-        };
-        
-        void on(string topic, const function<void()> &fn) {
-            Message msg(topic, nullopt, nullopt);
-            internal_on(msg, fn);
-        }
-
-        void on(string topic, string category, const function<void()> &fn) {
-            Message msg(topic, category, nullopt);
-            internal_on(msg, fn);
-        }
-
-        void on(string topic, string category, string subcategory, const function<void()> &fn) {
-            Message msg(topic, category, subcategory);
-            internal_on(msg, fn);
-        }
-
-        void on(Message message, const function<void()> &fn) {
-            internal_on(message, fn);
-        }
+        void drop(string topic);
+        void drop(string topic, string category);
+        void drop(string topic, string category, string subcategory);
 
     private:
-        class Subscription {
-            public:
-                Message message;
-                const function<void()> &func;
+        class Subscription;
+        unsigned int _index = 0;
+        vector<Subscription> _subscriptions_index;
+        unordered_map<string, vector<unsigned>> _subscriptions_topic;
+        unordered_map<string, vector<unsigned int>> _subscriptions_topic_subcategory;
+        unordered_map<string, vector<unsigned int>> _subscriptions_topic_category_subcategory;
 
-                Subscription(Message msg, const function<void()> &fn) : message(msg), func(fn) {};
-        };
-        vector<Subscription> subscriptions;
+        // Internal Utilities
+        Message internal_normalize(Message message);
 
+        // Internal APIs
         void internal_emit(Message message);
         bool internal_on(Message message, const function<void()> &fn);
+        bool internal_drop(Message message);
 
         void internal_subscribe(Message message);
         void internal_unsubscribe(Message message);
 };
 
+// Nested class definitions
+// Public
+struct Msngr::Message {
+    public:
+        Message() {};
+        Message(optional<string> topic, optional<string> category, optional<string> subcategory) : topicOpt(topic), categoryOpt(category), subcategoryOpt(subcategory) {};
+
+        optional<string> topicOpt = nullopt;
+        optional<string> categoryOpt = nullopt;
+        optional<string> subcategoryOpt = nullopt;
+};
+
+// Private
+class Msngr::Subscription {
+    public:
+        Message message;
+        const function<void()> &func;
+
+        Subscription(Message msg, const function<void()> &fn) : message(msg), func(fn) {};
+};
+
+// Msngr::* definitions
+// Public
+void Msngr::emit(string topic) {
+    Message msg(topic, nullopt, nullopt);
+    internal_emit(msg);
+};
+
+void Msngr::emit(string topic, string category) {
+    Message msg(topic, category, nullopt);
+    internal_emit(msg);
+};
+
+void Msngr::emit(string topic, string category, string subcategory) {
+    Message msg(topic, category, subcategory);
+    internal_emit(msg);
+};
+
+void Msngr::on(string topic, const function<void()> &fn) {
+    Message msg(topic, nullopt, nullopt);
+    internal_on(msg, fn);
+};
+
+void Msngr::on(string topic, string category, const function<void()> &fn) {
+    Message msg(topic, category, nullopt);
+    internal_on(msg, fn);
+};
+
+void Msngr::on(string topic, string category, string subcategory, const function<void()> &fn) {
+    Message msg(topic, category, subcategory);
+    internal_on(msg, fn);
+};
+
+void Msngr::drop(string topic) {
+
+};
+
+void Msngr::drop(string topic, string category) {
+
+};
+
+void Msngr::drop(string topic, string category, string subcategory) {
+
+};
+
+// Private Utilities
+Msngr::Message Msngr::internal_normalize(Message message) {
+    Message normalized;
+    if (message.topicOpt != nullopt) {
+        string val = message.topicOpt.value();
+        transform(val.begin(), val.end(), val.begin(), ::tolower);
+        normalized.topicOpt = optional<string>(val);
+    }
+
+    if (message.categoryOpt != nullopt) {
+        string val = message.categoryOpt.value();
+        transform(val.begin(), val.end(), val.begin(), ::tolower);
+        normalized.categoryOpt = optional<string>(val);
+    }
+
+    if (message.subcategoryOpt != nullopt) {
+        string val = message.subcategoryOpt.value();
+        transform(val.begin(), val.end(), val.begin(), ::tolower);
+        normalized.subcategoryOpt = optional<string>(val);
+    }
+
+    return normalized;
+};
+
+// Private APIs
 void Msngr::internal_emit(Message message) {
-    for (Subscription sub : subscriptions) {
+    message = internal_normalize(message);
+    /*for (Subscription sub : subscriptions) {
         if (message.topicOpt && message.topicOpt.value() == sub.message.topicOpt.value()) {
             sub.func();
         }
-    }
+    }*/
 };
 
 bool Msngr::internal_on(Message message, const function<void()> &fn) {
     Subscription sub(message, fn);
-    subscriptions.push_back(sub);
+
+    
     return true;
+};
+
+bool Msngr::internal_drop(Message message) {
+
 };
 
 #endif
